8b8ad3a25a5bf72b5df21093d74c207f07ce3512
Chinmay Gulhane <chinmay.gulhane@gmail.com>
2024-01-21 16:12:56 -0500

    Database connection setup

--- server/index.js
+++ server/index.js
@@ -1,7 +1,23 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 
+// dotenv package installed to store mongo url in env file
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("Connected to MongoDb!");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+// Initialize express app
 const app = express();
 
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running successfully on ${PORT}`);
 });
