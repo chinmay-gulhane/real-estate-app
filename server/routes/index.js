64271b880095ff0cf438e4a941e429b2cc692d11
Chinmay Gulhane <chinmay.gulhane@gmail.com>
2024-01-21 17:30:52 -0500

    Created middleware for error handling

--- server/routes/index.js
+++ server/routes/index.js
@@ -4,4 +4,15 @@ import userRouter from "./user.routes.js";
 export default (app) => {
   app.use("/api/user", userRouter);
   app.use("/api/auth", authRouter);
+
+  // middleware for auth
+  app.use((err, req, res, next) => {
+    const statusCode = err.statusCode || 500;
+    const message = err.message || "Internal Server Error";
+    return res.status(statusCode).json({
+      success: false,
+      statusCode,
+      message,
+    });
+  });
 };
