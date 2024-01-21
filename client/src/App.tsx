89937622d24d29ee856e8b17a6a0e34baf041ce4
Chinmay Gulhane <chinmay.gulhane@gmail.com>
2024-01-21 12:02:33 -0500

    Added intial setup for react and tailwind

---
+++ client/src/App.tsx
@@ -0,0 +1,15 @@
+import "./index.css";
+
+function App() {
+  return (
+    <div className="bg-gray-200 p-4">
+      <h1 className="text-2xl font-bold text-blue-600">Hello, Tailwind CSS!</h1>
+      <h1 className="text-2xl font-bold text-red-400">Hello, Tailwind CSS!</h1>
+      <h1 className="text-2xl font-bold text-red-400">Hello, Tailwind CSS!</h1>
+      <h1 className="text-2xl font-bold text-red-400">Hello, Tailwind CSS!</h1>
+      <h1 className="text-2xl font-bold text-red-950">Hello, Tailwind CSS!</h1>
+    </div>
+  );
+}
+
+export default App;
