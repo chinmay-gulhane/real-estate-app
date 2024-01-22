2a9037786b23f56b76f554bf414c3af5f8760488
Chinmay Gulhane <chinmay.gulhane@gmail.com>
2024-01-22 10:30:22 -0500

    Created sign in page

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -23,7 +23,7 @@ export const signin = async (req, res, next) => {
     const validUser = await User.findOne({ email });
     if (!validUser) return next(errorHandler(404, "User not found!"));
     const validPassword = bcryptjs.compareSync(password, validUser.password);
-    if (!validUser) return next(errorHandler(404, "Wrong credentials!"));
+    if (!validPassword) return next(errorHandler(404, "Wrong credentials!"));
     const token = jwt.sign({ id: validPassword._id }, process.env.JWT_SECRET);
     // removing password from response
     const { password: pass, ...rest } = validUser._doc;
